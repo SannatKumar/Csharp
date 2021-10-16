4f17e43fbe9766e41d074c5b823ff8310dda3e61
Raj Kumar <interplaytheheads@gmail.com>
2021-10-16 20:23:39 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Firstprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Print();
+
+            
+
+        }
+        void Print()
+        {
+            Console.WriteLine("Some Statement");
+        }
+    }
+}
